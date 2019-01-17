95d1f2740e80f0c0a7edb571e7a1734717be4110
snkoski <shawnnkoski@gmail.com>
2019-01-17 16:05:14 -0500

    Finished page_font_style extension.

--- budget_manager/eventPage.js
+++ budget_manager/eventPage.js
@@ -37,5 +37,4 @@ chrome.contextMenus.onClicked.addListener(function(clickData){
 
 chrome.storage.onChanged.addListener(function(changes, storageName){
   chrome.browserAction.setBadgeText({"text": changes.total.newValue.toString()});
-  // chrome.browserAction.setBadgeText({"text": "1"});
 })
